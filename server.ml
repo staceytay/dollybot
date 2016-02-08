4cd07c274ec5aa8769d627b3f10be4d9176b51ea
staceytay <stay.jw@gmail.com>
2016-02-09 02:00:18 +0800

    Parse command and almost send Dolly photos

--- server.ml
+++ server.ml
@@ -1,24 +1,36 @@
 open Core.Std
 open Async.Std
-open Telegram_t
+open Telegram_j
 
 type command =
   | Dolly
 
 let command_of_string str =
-  if str = "dolly" then Dolly
+  if str = "/dolly" then Dolly
   else failwith "Invalid command"
+
+let random_dolly_photo () =
+  let dolly_photos = ["<file_id1>"; "<file_id2>"]
+  in List.nth_exn dolly_photos (Random.int (List.length dolly_photos))
 
 let response_from_update { update_id = _; message = maybe; } =
   match maybe with
   | Some msg ->
-     begin match msg.text with
-           | Some text ->
-              let chat_id = (msg.chat).id
-              in let s_msg = { chat_id = chat_id; text = text; }
-              in let response = `String (Telegram_j.string_of_send_message s_msg)
-              in Cohttp_async.Server.respond ~body:response `OK
-           | None -> failwith "No text in update"
+     begin
+       match msg.text with
+       | Some text ->
+          let tokens = Str.bounded_split (Str.regexp "[ ]+") text 2
+          in let command = command_of_string (List.hd_exn tokens)
+          in begin
+              match command with
+              | Dolly ->
+                 let photo_r = { chat_id = msg.chat.id;
+                                 photo = random_dolly_photo ();
+                                 caption = None; }
+                 in let response = `String (string_of_photo_reply photo_r)
+                 in Cohttp_async.Server.respond ~body:response `OK
+             end
+       | None -> failwith "No text in update"
      end
   | None -> failwith "No message in update"
 
@@ -32,7 +44,7 @@ let run ~port =
           Cohttp_async.Body.to_string body
           >>= (fun body ->
                Log.Global.info "POST: %s" body;
-               response_from_update (Telegram_j.update_of_string body))
+               response_from_update (update_of_string body))
        | _ -> Cohttp_async.Server.respond `Method_not_allowed
     )
   >>= fun _ -> Deferred.never ()
