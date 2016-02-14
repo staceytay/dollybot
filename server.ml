2afd6aa719f9022dd4a2d78059d042a1d8ed1c02
staceytay <stay.jw@gmail.com>
2016-02-14 17:01:21 +0800

    Reindent with ocp-indent

--- server.ml
+++ server.ml
@@ -16,22 +16,22 @@ let random_dolly_photo () =
 let response_from_update { update_id = _; message = maybe; } =
   match maybe with
   | Some msg ->
-     begin
-       match msg.text with
-       | Some text ->
-          let tokens = Str.bounded_split (Str.regexp "[ ]+") text 2
-          in let command = command_of_string (List.hd_exn tokens)
-          in begin
-              match command with
-              | Dolly ->
-                 let photo_r = { chat_id = msg.chat.id;
-                                 photo = random_dolly_photo ();
-                                 caption = None; }
-                 in let response = `String (string_of_photo_reply photo_r)
-                 in Cohttp_async.Server.respond ~body:response `OK
-             end
-       | None -> failwith "No text in update"
-     end
+    begin
+      match msg.text with
+      | Some text ->
+        let tokens = Str.bounded_split (Str.regexp "[ ]+") text 2
+        in let command = command_of_string (List.hd_exn tokens)
+        in begin
+          match command with
+          | Dolly ->
+            let photo_r = { chat_id = msg.chat.id;
+                            photo = random_dolly_photo ();
+                            caption = None; }
+            in let response = `String (string_of_photo_reply photo_r)
+            in Cohttp_async.Server.respond ~body:response `OK
+        end
+      | None -> failwith "No text in update"
+    end
   | None -> failwith "No message in update"
 
 let run ~port =
@@ -41,10 +41,10 @@ let run ~port =
     (fun ~body _ req ->
        match (Cohttp.Request.meth req) with
        | `POST ->
-          Cohttp_async.Body.to_string body
-          >>= (fun body ->
-               Log.Global.info "POST: %s" body;
-               response_from_update (update_of_string body))
+         Cohttp_async.Body.to_string body
+         >>= (fun body ->
+             Log.Global.info "POST: %s" body;
+             response_from_update (update_of_string body))
        | _ -> Cohttp_async.Server.respond `Method_not_allowed
     )
   >>= fun _ -> Deferred.never ()
@@ -55,7 +55,7 @@ let () =
     Command.Spec.(
       empty
       +> flag "-port" (optional_with_default 8080 int)
-         ~doc: " Port to listen on (default 8080)"
+        ~doc: " Port to listen on (default 8080)"
     )
     (fun port () -> run ~port)
   |> Command.run
